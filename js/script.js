fdea63920a1ea353658108c9806941dccdb5ec8d
Andrés <marlon_rivera95@hotmail.com>
2022-08-12 16:29:48 -0500

    subidita222

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ var swiper = new Swiper(".home-slider", {
 
 //lo más vendido
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  // slidesPerView: 3,
   spaceBetween: 55,
   autoplay:{
     delay:3000,
@@ -41,4 +41,21 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 60
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    500: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
 });
